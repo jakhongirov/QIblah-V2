cadf6cbe65df12e2cad52ca03f5eabf797a20d9e
jakhongirov <johongirovdiyorbek08@gmail.com>
2024-06-29 20:29:44 +0500

    add

--- src/modules/payment/check.js
+++ src/modules/payment/check.js
@@ -7,21 +7,26 @@ module.exports = {
          const foundUser = await model.foundUser(user_id)
 
          if (foundUser) {
-            if (foundUser?.user_country_code?.toLowerCase() == "uz") {
-               return res.status(200).json({
-                  status: 200,
-                  message: "Success",
-                  data: {
-                     click: `https://my.click.uz/services/pay?merchant_id=26420&service_id=34442&transaction_param=Qiblah&additional_param3=${foundUser?.user_id}`,
-                     uzum: ""
-                  }
-               })
-            } else {
-               return res.status(200).json({
-                  status: 200,
-                  message: "ok"
-               })
-            }
+            // if (foundUser?.user_country_code?.toLowerCase() == "uz") {
+            //    return res.status(200).json({
+            //       status: 200,
+            //       message: "Success",
+            //       data: {
+            //          click: `https://my.click.uz/services/pay?merchant_id=26420&service_id=34442&transaction_param=Qiblah&additional_param3=${foundUser?.user_id}`,
+            //          uzum: ""
+            //       }
+            //    })
+            // } else {
+            //    return res.status(200).json({
+            //       status: 200,
+            //       message: "ok"
+            //    })
+            // }
+
+            return res.status(200).json({
+               status: 200,
+               message: "ok"
+            })
          } else {
             return res.status(404).json({
                status: 404,
