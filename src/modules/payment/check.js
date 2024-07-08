f2fb219ddb3ab6a9b263f0aa2aba1d1953ccf55c
jakhongirov <johongirovdiyorbek08@gmail.com>
2024-07-08 17:09:01 +0500

    add

--- src/modules/payment/check.js
+++ src/modules/payment/check.js
@@ -7,26 +7,23 @@ module.exports = {
          const foundUser = await model.foundUser(user_id)
 
          if (foundUser) {
-            // if (foundUser?.user_country_code?.toLowerCase() == "uz") {
-            //    return res.status(200).json({
-            //       status: 200,
-            //       message: "Success",
-            //       data: {
-            //          click: `https://my.click.uz/services/pay?merchant_id=26420&service_id=34442&transaction_param=Qiblah&additional_param3=${foundUser?.user_id}`,
-            //          uzum: ""
-            //       }
-            //    })
-            // } else {
-            //    return res.status(200).json({
-            //       status: 200,
-            //       message: "ok"
-            //    })
-            // }
+            const checkOs = foundUser?.user_os?.includes('2')
 
-            return res.status(200).json({
-               status: 200,
-               message: "ok"
-            })
+            if (foundUser?.user_country_code?.toLowerCase() == "uz" && !checkOs) {
+               return res.status(200).json({
+                  status: 200,
+                  message: "Success",
+                  data: {
+                     click: `https://my.click.uz/services/pay?merchant_id=26420&service_id=34442&transaction_param=Qiblah&additional_param3=${foundUser?.user_id}`,
+                     uzum: ""
+                  }
+               })
+            } else {
+               return res.status(200).json({
+                  status: 200,
+                  message: "ok"
+               })
+            }
          } else {
             return res.status(404).json({
                status: 404,
