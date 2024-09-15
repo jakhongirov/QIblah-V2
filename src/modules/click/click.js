2431af995aa70964fd93dfa59aa73b3463d19638
jakhongirov <johongirovdiyorbek08@gmail.com>
2024-09-15 19:26:09 +0500

    add

--- src/modules/click/click.js
+++ src/modules/click/click.js
@@ -1,6 +1,7 @@
 const model = require('./model')
 const iconv = require('iconv-lite');
 const bot = require('../../lib/bot')
+const axios = require("axios")
 
 module.exports = {
    Prepare: async (req, res) => {
@@ -58,79 +59,98 @@ module.exports = {
 
    Complete: async (req, res) => {
       try {
-         const { click_trans_id, merchant_trans_id, error, error_note } = req.body
+         const { click_trans_id, merchant_trans_id, amount, param2, param3, error, error_note } = req.body
 
          console.log("click cp", req.body)
 
          if (error_note === 'Success') {
-            let rate = {}
-            let tracking = {}
-            const foundTrans = await model.foundTrans(click_trans_id)
-            const foundPayment = await model.foundPayment(foundTrans?.tarif);
 
-            if (foundPayment) {
-               rate = foundPayment
+            if (merchant_trans_id == "Qiblah") {
+               let rate = {}
+               let tracking = {}
+               const foundTrans = await model.foundTrans(click_trans_id)
+               const foundPayment = await model.foundPayment(foundTrans?.tarif);
+
+               if (foundPayment) {
+                  rate = foundPayment
+               } else {
+                  const hexString = foundTrans?.tarif?.replace(/%/g, '');
+                  const buffer = Buffer.from(hexString, 'hex');
+                  const decoded = iconv.decode(buffer, 'windows-1251');
+                  const foundPayment = await model.foundPayment(decoded);
+                  rate = foundPayment
+               }
+
+               const today = new Date();
+               const expiresDate = new Date(today);
+               const monthToAdd = Number(rate?.month);
+               let targetMonth = today.getMonth() + monthToAdd;
+               let targetYear = today.getFullYear();
+
+               while (targetMonth > 11) {
+                  targetMonth -= 12;
+                  targetYear++;
+               }
+
+               expiresDate.setFullYear(targetYear, targetMonth, 1);
+               const maxDaysInTargetMonth = new Date(targetYear, targetMonth + 1, 0).getDate();
+               expiresDate.setDate(Math.min(today.getDate(), maxDaysInTargetMonth));
+
+               if (expiresDate < today) {
+                  expiresDate.setMonth(expiresDate.getMonth() + 1);
+                  expiresDate.setDate(0); // Set to the last day of the previous month
+               }
+               const options = {
+                  year: 'numeric',
+                  month: '2-digit',
+                  day: '2-digit',
+                  hour: '2-digit',
+                  minute: '2-digit',
+                  second: '2-digit',
+                  timeZone: 'Asia/Tashkent'
+               };
+               const formatter = new Intl.DateTimeFormat('en-GB', options);
+               const formattedDate2 = formatter.format(today);
+
+               // Replace the format to match the required "DD.MM.YYYY HH:MM:SS" format
+               const [day, month, year] = formattedDate2.split(', ')[0].split('/');
+               const time = formattedDate2.split(', ')[1];
+               const finalFormat = `${day}.${month}.${year} ${time}`;
+
+               const formattedDate = expiresDate.toISOString();
+
+               tracking['tarif'] = rate?.category_name
+               tracking['amount'] = foundTrans?.amount
+               tracking['trans_id'] = click_trans_id
+               tracking['date'] = finalFormat
+               tracking['expire_date'] = formattedDate
+               tracking['type'] = "click"
+
+               const foundUser = await model.foundUser(foundTrans?.user_id)
+               await model.editUserPremium(foundUser?.user_token[Number(foundUser?.user_token?.length - 1)], formattedDate, "click", tracking)
+               await model.editTrans(click_trans_id, 'paid')
+
+               bot.sendMessage(634041736,
+                  `<strong>Click:</strong>\n\nUser token:${foundUser?.user_token[foundUser?.user_token?.length - 1]}\nUser id: ${foundTrans?.user_id}\nTarif: ${foundPayment?.category_name}\nAmount: ${foundTrans?.amount}\nDate: ${finalFormat}`,
+                  { parse_mode: "HTML" }
+               );
             } else {
-               const hexString = foundTrans?.tarif?.replace(/%/g, '');
-               const buffer = Buffer.from(hexString, 'hex');
-               const decoded = iconv.decode(buffer, 'windows-1251');
-               const foundPayment = await model.foundPayment(decoded);
-               rate = foundPayment
+               const url = 'http://185.217.131.150:777/api/v1/invoice'; // The API endpoint
+               const data = {
+                  userId: param2,
+                  botId: merchant_trans_id,
+                  amount: amount,
+                  tariffId: param3
+               }; // The data you want to send
+
+               axios.post(url, data)
+                  .then(response => {
+                     console.log('Response Data:', response.data);
+                  })
+                  .catch(error => {
+                     console.error('Error:', error);
+                  });
             }
-
-            const today = new Date();
-            const expiresDate = new Date(today);
-            const monthToAdd = Number(rate?.month);
-            let targetMonth = today.getMonth() + monthToAdd;
-            let targetYear = today.getFullYear();
-
-            while (targetMonth > 11) {
-               targetMonth -= 12;
-               targetYear++;
-            }
-
-            expiresDate.setFullYear(targetYear, targetMonth, 1);
-            const maxDaysInTargetMonth = new Date(targetYear, targetMonth + 1, 0).getDate();
-            expiresDate.setDate(Math.min(today.getDate(), maxDaysInTargetMonth));
-
-            if (expiresDate < today) {
-               expiresDate.setMonth(expiresDate.getMonth() + 1);
-               expiresDate.setDate(0); // Set to the last day of the previous month
-            }
-            const options = {
-               year: 'numeric',
-               month: '2-digit',
-               day: '2-digit',
-               hour: '2-digit',
-               minute: '2-digit',
-               second: '2-digit',
-               timeZone: 'Asia/Tashkent'
-            };
-            const formatter = new Intl.DateTimeFormat('en-GB', options);
-            const formattedDate2 = formatter.format(today);
-
-            // Replace the format to match the required "DD.MM.YYYY HH:MM:SS" format
-            const [day, month, year] = formattedDate2.split(', ')[0].split('/');
-            const time = formattedDate2.split(', ')[1];
-            const finalFormat = `${day}.${month}.${year} ${time}`;
-
-            const formattedDate = expiresDate.toISOString();
-
-            tracking['tarif'] = rate?.category_name
-            tracking['amount'] = foundTrans?.amount
-            tracking['trans_id'] = click_trans_id
-            tracking['date'] = finalFormat
-            tracking['expire_date'] = formattedDate
-            tracking['type'] = "click"
-
-            const foundUser = await model.foundUser(foundTrans?.user_id)
-            await model.editUserPremium(foundUser?.user_token[Number(foundUser?.user_token?.length - 1)], formattedDate, "click", tracking)
-            await model.editTrans(click_trans_id, 'paid')
-
-            bot.sendMessage(634041736,
-               `<strong>Click:</strong>\n\nUser token:${foundUser?.user_token[foundUser?.user_token?.length - 1]}\nUser id: ${foundTrans?.user_id}\nTarif: ${foundPayment?.category_name}\nAmount: ${foundTrans?.amount}\nDate: ${finalFormat}`,
-               { parse_mode: "HTML" }
-            );
          }
 
          return res.status(200).json({
