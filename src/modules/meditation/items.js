d4efd135cb2fbfcc9d566bf66e432b62c9373f85
jakhongirov <johongirovdiyorbek08@gmail.com>
2024-09-23 20:18:29 +0500

    add

--- src/modules/meditation/items.js
+++ src/modules/meditation/items.js
@@ -158,8 +158,8 @@ module.exports = {
          const file = JSON.parse(data.read())
 
          for (const item of file) {
-            // const audioPath = item?.audioLink?.split("https://server.qiblah.app/")[1]
-            const audioPath = path.resolve(__dirname, '..', '..', 'public', 'images', `${item?.audioLink?.split("https://server.qiblah.app/public/images/")}`)
+            const audioFileName = item?.audioLink?.split("https://server.qiblah.app/public/images/")[1]; // Get the file name
+            const audioPath = path.resolve(__dirname, '..', '..', '..', 'public', 'images', audioFileName); // Construct the full path
             const duration = await getMp3Duration(audioPath);
             console.log(item?.name)
 
