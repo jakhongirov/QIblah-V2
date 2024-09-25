054d23651c4500d11db822b15fd0e1bc0cd25f97
jakhongirov <johongirovdiyorbek08@gmail.com>
2024-09-25 12:25:58 +0500

    add

---
+++ src/modules/pray/categories/categories.js
@@ -0,0 +1,278 @@
+const model = require('./model')
+const path = require('path')
+const FS = require('../../../lib/fs/fs')
+
+module.exports = {
+   GET: async (req, res) => {
+      try {
+         const { lang_id, gender } = req.query
+
+         if (lang_id && gender) {
+            const categoriesByLangGender = await model.categories(lang_id, gender)
+
+            if (categoriesByLangGender?.length > 0) {
+               return res.status(200).json({
+                  status: 200,
+                  message: "Success",
+                  data: categoriesByLangGender
+               })
+            } else {
+               return res.status(404).json({
+                  status: 404,
+                  message: "Not found"
+               })
+            }
+         } else if (lang_id) {
+            const categoriesByLang = await model.categories(lang_id)
+
+            if (categoriesByLang?.length > 0) {
+               return res.status(200).json({
+                  status: 200,
+                  message: "Success",
+                  data: categoriesByLang
+               })
+            } else {
+               return res.status(404).json({
+                  status: 404,
+                  message: "Not found"
+               })
+            }
+         } else {
+            const categories = await model.categories(lang_id)
+
+            if (categories?.length > 0) {
+               return res.status(200).json({
+                  status: 200,
+                  message: "Success",
+                  data: categories
+               })
+            } else {
+               return res.status(404).json({
+                  status: 404,
+                  message: "Not found"
+               })
+            }
+         }
+
+      } catch (error) {
+         console.log(error);
+         res.status(500).json({
+            status: 500,
+            message: "Interval Server Error"
+         })
+      }
+   },
+
+   ADD_FILE: async (req, res) => {
+      try {
+         const data = new FS(path.resolve(__dirname, '..', '..', '..', '..', 'files', ``))
+         const file = JSON.parse(data.read())
+
+         for (const item of file) {
+            console.log(item?.title)
+
+            await model.addCategoryFile(
+               item?.id,
+               item?.title,
+               item?.description,
+               item?.titleColor,
+               item?.descriptionColor,
+               item?.backgroundColor,
+               item?.isBig,
+               item?.gender,
+               item?.order,
+               item?.langID,
+               item?.image ? item?.image : '',
+               item?.image ? item?.image?.split('https://srvr.qiblah.app/public/images/')[1] : '',
+            )
+         }
+
+         console.log('stop')
+
+         return res.status(200).json({
+            status: 200,
+            message: "Success"
+         });
+
+      } catch (error) {
+         console.log(error);
+         res.status(500).json({
+            status: 500,
+            message: "Interval Server Error"
+         })
+      }
+   },
+
+   ADD_CATEGORY: async (req, res) => {
+      try {
+         const uploadPhoto = req.file;
+         const {
+            category_name,
+            category_description,
+            category_text_color,
+            category_description_color,
+            category_background_color,
+            category_big,
+            category_gender,
+            category_order,
+            lang_id
+         } = req.body
+         const imgUrl = `${process.env.BACKEND_URL}/${uploadPhoto?.filename}`;
+         const imgName = uploadPhoto?.filename;
+
+         const addCategory = await model.addCategory(
+            category_name,
+            category_description,
+            category_text_color,
+            category_description_color,
+            category_background_color,
+            category_big,
+            category_gender,
+            category_order,
+            lang_id,
+            imgUrl,
+            imgName
+         )
+
+         if (addCategory) {
+            return res.status(201).json({
+               status: 201,
+               message: "Created",
+               data: addCategory
+            })
+         } else {
+            return res.status(400).json({
+               status: 400,
+               message: "Bad request"
+            })
+         }
+
+      } catch (error) {
+         console.log(error);
+         res.status(500).json({
+            status: 500,
+            message: "Interval Server Error"
+         })
+      }
+   },
+
+   EDIT_CATEGORY: async (req, res) => {
+      try {
+         const uploadPhoto = req.file;
+         const {
+            category_id,
+            category_name,
+            category_description,
+            category_text_color,
+            category_description_color,
+            category_background_color,
+            category_big,
+            category_gender,
+            category_order,
+            lang_id
+         } = req.body
+
+         const foundCategory = await model.foundCategory(category_id)
+
+         if (foundCategory) {
+            let imgUrl = '';
+            let imgName = '';
+
+            if (uploadPhoto) {
+               if (foundCategory?.category_image_name) {
+                  const deleteOldAvatar = new FS(path.resolve(__dirname, '..', '..', '..', '..', 'public', 'images', `${foundCategory?.category_image_name}`))
+                  deleteOldAvatar.delete()
+               }
+               imgUrl = `${process.env.BACKEND_URL}/${uploadPhoto?.filename}`;
+               imgName = uploadPhoto?.filename;
+            } else {
+               imgUrl = foundCategory?.category_image_link;
+               imgName = foundCategory?.category_image_name;
+            }
+
+            const editCategory = await model.editCategory(
+               category_id,
+               category_name,
+               category_description,
+               category_text_color,
+               category_description_color,
+               category_background_color,
+               category_big,
+               category_gender,
+               category_order,
+               lang_id,
+               imgUrl,
+               imgName
+            )
+
+            if (editCategory) {
+               return res.status(200).json({
+                  status: 200,
+                  message: "Success",
+                  data: editCategory
+               })
+            } else {
+               return res.status(400).json({
+                  status: 400,
+                  message: "Bad request"
+               })
+            }
+         } else {
+            return res.status(404).json({
+               status: 404,
+               message: "Not found"
+            })
+         }
+
+      } catch (error) {
+         console.log(error);
+         res.status(500).json({
+            status: 500,
+            message: "Interval Server Error"
+         })
+      }
+   },
+
+   DELETE_CATEGORY: async (req, res) => {
+      try {
+         const { category_id } = req.body
+         const foundCategory = await model.foundCategory(category_id)
+
+         if (foundCategory) {
+            const deleteCategory = await model.deleteCategory(category_id)
+
+            if (deleteCategory) {
+
+               if (foundCategory?.category_image_name) {
+                  const deleteOldAvatar = new FS(path.resolve(__dirname, '..', '..', '..', '..', 'public', 'images', `${foundCategory?.category_image_name}`))
+                  deleteOldAvatar.delete()
+               }
+
+               return res.status(200).json({
+                  status: 200,
+                  message: "Success",
+                  data: deleteCategory
+               })
+            } else {
+               return res.status(400).json({
+                  status: 400,
+                  message: "Bad request"
+               })
+            }
+
+         } else {
+            return res.status(404).json({
+               status: 404,
+               message: "Not found"
+            })
+         }
+
+      } catch (error) {
+         console.log(error);
+         res.status(500).json({
+            status: 500,
+            message: "Interval Server Error"
+         })
+      }
+   }
+}
---
+++ src/modules/pray/subCategories/subCategories.js
@@ -0,0 +1,237 @@
+const model = require('./model')
+const path = require('path')
+const FS = require('../../../lib/fs/fs')
+
+module.exports = {
+   GET: async (req, res) => {
+      try {
+         const { category_id } = req.query
+
+         if (category_id) {
+            const subCategoriesByCategoryID = await model.subCategories(category_id)
+
+            if (subCategoriesByCategoryID?.length > 0) {
+               return res.status(200).json({
+                  status: 200,
+                  message: "Success",
+                  data: subCategoriesByCategoryID
+               })
+            } else {
+               return res.status(404).json({
+                  status: 404,
+                  message: "Not found"
+               })
+            }
+         } else {
+            const subCategories = await model.subCategories()
+
+            if (subCategories?.length > 0) {
+               return res.status(200).json({
+                  status: 200,
+                  message: "Success",
+                  data: subCategories
+               })
+            } else {
+               return res.status(404).json({
+                  status: 404,
+                  message: "Not found"
+               })
+            }
+         }
+
+      } catch (error) {
+         console.log(error);
+         res.status(500).json({
+            status: 500,
+            message: "Interval Server Error"
+         })
+      }
+   },
+
+   ADD_FILE: async (req, res) => {
+      try {
+         const data = new FS(path.resolve(__dirname, '..', '..', '..', '..', 'files', ``))
+         const file = JSON.parse(data.read())
+
+         for (const item of file) {
+            console.log(item?.title)
+
+            await model.addSubCategoriesFile(
+               item?.id,
+               item?.title,
+               item?.haveItems,
+               item?.isNumeric,
+               item?.categoryID,
+               item?.icon ? item?.icon : "",
+               item?.icon ? item?.icon?.split("https://srvr.qiblah.app/public/images/")[1] : "",
+            )
+         }
+
+         console.log('stop')
+
+         return res.status(200).json({
+            status: 200,
+            message: "Success"
+         });
+
+      } catch (error) {
+         console.log(error);
+         res.status(500).json({
+            status: 500,
+            message: "Interval Server Error"
+         })
+      }
+   },
+
+   ADD_SUB_CATEGORIES: async (req, res) => {
+      try {
+         const uploadPhoto = req.file;
+         const {
+            sub_category_name,
+            have_item,
+            numeric,
+            category_id
+         } = req.body
+         const imgUrl = `${process.env.BACKEND_URL}/${uploadPhoto?.filename}`;
+         const imgName = uploadPhoto?.filename;
+
+         const addSubCategories = await model.addSubCategories(
+            sub_category_name,
+            have_item,
+            numeric,
+            category_id,
+            imgUrl,
+            imgName
+         )
+
+         if (addSubCategories) {
+            return res.status(201).json({
+               status: 201,
+               message: "Created",
+               data: addSubCategories
+            })
+         } else {
+            return res.status(400).json({
+               status: 400,
+               message: "Bad request"
+            })
+         }
+
+      } catch (error) {
+         console.log(error);
+         res.status(500).json({
+            status: 500,
+            message: "Interval Server Error"
+         })
+      }
+   },
+
+   EDIT_SUB_CATEGORIES: async (req, res) => {
+      try {
+         const uploadPhoto = req.file;
+         const {
+            sub_category_id,
+            sub_category_name,
+            have_item,
+            numeric,
+            category_id
+         } = req.body
+         const foundSubCategory = await model.foundSubCategory(sub_category_id)
+
+         if (foundSubCategory) {
+            let imgUrl = '';
+            let imgName = '';
+
+            if (uploadPhoto) {
+               if (foundSubCategory?.sub_category_image_name) {
+                  const deleteOldAvatar = new FS(path.resolve(__dirname, '..', '..', '..', '..', 'public', 'images', `${foundSubCategory?.sub_category_image_name}`))
+                  deleteOldAvatar.delete()
+               }
+               imgUrl = `${process.env.BACKEND_URL}/${uploadPhoto?.filename}`;
+               imgName = uploadPhoto?.filename;
+            } else {
+               imgUrl = foundSubCategory?.sub_category_image_link;
+               imgName = foundSubCategory?.sub_category_image_name;
+            }
+
+            const editSubCategory = await model.editSubCategory(
+               sub_category_id,
+               sub_category_name,
+               have_item,
+               numeric,
+               category_id,
+               imgUrl,
+               imgName
+            )
+
+            if (editSubCategory) {
+               return res.status(200).json({
+                  status: 200,
+                  message: "Success",
+                  data: editSubCategory
+               })
+            } else {
+               return res.status(400).json({
+                  status: 400,
+                  message: "Bad request"
+               })
+            }
+         } else {
+            return res.status(404).json({
+               status: 404,
+               message: "Not found"
+            })
+         }
+
+      } catch (error) {
+         console.log(error);
+         res.status(500).json({
+            status: 500,
+            message: "Interval Server Error"
+         })
+      }
+   },
+
+   DELETE_SUB_CATEGORIES: async (req, res) => {
+      try {
+         const { sub_category_id } = req.body
+         const foundSubCategory = await model.foundSubCategory(sub_category_id)
+
+         if (foundSubCategory) {
+            const deleteSubCategory = await model.deleteSubCategory(sub_category_id)
+
+            if (deleteSubCategory) {
+
+               if (foundSubCategory?.sub_category_image_name) {
+                  const deleteOldAvatar = new FS(path.resolve(__dirname, '..', '..', '..', '..', 'public', 'images', `${foundSubCategory?.sub_category_image_name}`))
+                  deleteOldAvatar.delete()
+               }
+
+               return res.status(200).json({
+                  status: 200,
+                  message: "Success",
+                  data: deleteSubCategory
+               })
+            } else {
+               return res.status(400).json({
+                  status: 400,
+                  message: "Bad request"
+               })
+            }
+
+         } else {
+            return res.status(404).json({
+               status: 404,
+               message: "Not found"
+            })
+         }
+
+      } catch (error) {
+         console.log(error);
+         res.status(500).json({
+            status: 500,
+            message: "Interval Server Error"
+         })
+      }
+   }
+}
