f72802f104faadbfeed73f435b8b20ee9e7e1339
jakhongirov <johongirovdiyorbek08@gmail.com>
2024-12-19 16:58:48 +0800

    addg

--- src/modules/audios/audios.js
+++ src/modules/audios/audios.js
@@ -100,6 +100,11 @@ module.exports = {
             )
          }
 
+         return res.status(200).json({
+            status: 200,
+            message: "Success"
+         });
+
       } catch (error) {
          console.log(error);
          res.status(500).json({
