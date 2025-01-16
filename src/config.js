ae7a004489f6816013a4b5feb50174e207405030
jakhongirov <johongirovdiyorbek08@gmail.com>
2025-01-17 00:31:42 +0800

    addg

--- src/config.js
+++ src/config.js
@@ -1,10 +1,5 @@
 const PORT = 9090;
-const connection = {
-   connectionStringEL: "postgres://sgeengir:U9FyXq00U5YN1h-ud-HRTc9aIC_0pSuA@tai.db.elephantsql.com/sgeengir",
-   // connectionStringEL: "postgres://jvesrinh:nH4LbnttMxxtujfDFolGSMp2pTt1Ro7Z@tyke.db.elephantsql.com/jvesrinh",
-};
 
 module.exports = {
-   PORT,
-   connection,
+   PORT
 };
